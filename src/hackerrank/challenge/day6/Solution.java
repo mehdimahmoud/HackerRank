e78df2635e6dc98be0b725ab534785a4ef88dce7
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-11-21 01:07:35 +0000

    Challenge Day6 : Solution - Java7

--- src/hackerrank/challenge/day6/Solution.java
+++ src/hackerrank/challenge/day6/Solution.java
@@ -9,7 +9,27 @@ public class Solution {
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt(); // number of TestCases
+
+        try {
+            int T = scan.nextInt(); // number of TestCases
+            String[] S = new String[T];
+            String S_even="", S_odd="";
+            if (T<1 || T>10) throw new Exception();
+            for (int j =0;j<=T-1;j++){
+                S[j] = scan.next();
+            }
+            for (String Sj:S) {
+                for (int i=0;i<Sj.length();i++){
+                    if(i%2==0) S_even = S_even.concat(String.valueOf(Sj.charAt(i)));
+                    if(i%2!=0) S_odd = S_odd.concat(String.valueOf(Sj.charAt(i)));
+                }
+                System.out.println(S_even+" "+S_odd);
+                S_even = "";
+                S_odd = "";
+            }
+        }catch (Exception e){
+            System.out.println("Invalid input");
+        }
 
     }
 }
