ce7258e5aeebdb789438683bedc0a8b7e6a38bc5
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-11-21 00:10:35 +0000

    Challenge Day5 : Solution

--- src/hackerrank/challenge/day5/Solution.java
+++ src/hackerrank/challenge/day5/Solution.java
@@ -1,7 +1,24 @@
 package hackerrank.challenge.day5;
 
+import java.util.Scanner;
+
 /**
  * Created by mehdi on 20/11/16.
  */
 public class Solution {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        try {
+            if(n>=2 && n<=20){
+                for (int i=1;i<=10;i++){
+                    System.out.println(n + " x " + i + " = "+ n*i);
+                }
+            }else throw new Exception();
+        } catch (Exception e) {
+            System.out.println("Invalid input.");
+        }
+    }
 }
