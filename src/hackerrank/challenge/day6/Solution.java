8663387be4a3c729c379c6e8719ac83e13d0a69e
mehdimahmoud <mahmoudmehdi@yahoo.fr>
2016-11-21 00:25:23 +0000

    Challenge Day6 : First commit

---
+++ src/hackerrank/challenge/day6/Solution.java
@@ -0,0 +1,15 @@
+package hackerrank.challenge.day6;
+
+import java.util.Scanner;
+
+/**
+ * Created by mehdi on 21/11/16.
+ */
+public class Solution {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt(); // number of TestCases
+
+    }
+}
