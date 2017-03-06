e7bb1fd530eaca6a1f912540ae6aa7c727f796b6
Mehdi MAHMOUD <mehdi.mahmoud@gmail.com>
2017-03-06 09:41:13 +0000

    Solution exercice 3

--- src/main/java/hackerrank/algorithms/ex3/Solution.java
+++ src/main/java/hackerrank/algorithms/ex3/Solution.java
@@ -30,15 +30,36 @@ package hackerrank.algorithms.ex3;
  * When we add several integer values, the resulting sum might exceed the above range. You might need to use long long int in C/C++ or long data type in Java to store such sums.
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.LongStream;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // Solution in Java 7
+        // Solution in Java 8
+        Integer N = in.nextInt();
+        List<Long> A = new ArrayList<>();
 
 
+        // Gets inputs:
+        if(N<0 || N>10 || !(N instanceof Integer))
+            throw new IllegalArgumentException("You can only entry up to 10 integers.");
+
+        LongStream
+                .range(0, N)
+                .forEach(value -> {
+                    Long nb = in.nextLong();
+                    if(nb<0 || nb>Math.pow(10,10) || !(nb instanceof Long)) {
+                        throw new IllegalArgumentException("The numbers are Integer type. They should be included between 0 and 10^10.");
+                    }else A.add(nb);
+                });
+
+
+        // Sum
+        System.out.println(A.stream().reduce(0L, (acc, a) -> Math.addExact(acc, a)));
     }
 }
