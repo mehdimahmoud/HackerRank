046db1953b7d3abcbd76597fd6513952561cf260
Mehdi MAHMOUD <mehdi.mahmoud@gmail.com>
2017-03-18 17:05:17 +0000

    Solution Ex12 : Melodious password

--- src/main/java/hackerrank/algorithms/ex12/Solution.java
+++ src/main/java/hackerrank/algorithms/ex12/Solution.java
@@ -1,152 +1,124 @@
-package hackerrank.algorithms.ex10;
+package hackerrank.algorithms.ex12;
 /**
- * Created by mmik on 14/03/2017.
- * source: https://www.hackerrank.com/contests/w30/challenges/find-the-minimum-number
- * Jessica is learning to code and was recently introduced to the  function. This function compares two integers and returns the smaller one. This is what calling the function looks like when comparing two integers  and :
+ * Created by mmik on 17/03/2017.
+ * source: https://www.hackerrank.com/contests/w30/challenges/poles
+ * Kevin was thinking about telephone poles and came up with an idea for a fun programming challenge.
+ * There are n telephone poles ascending a mountain and each pole has a weight and a unique altitude.
+ * Our program must move the poles into k number of stacks, but we can only rearrange the poles
+ * according to certain criteria:
  * <p>
- * min(a, b)
- * Jessica realizes that she can also find the smallest of three integers a, b, c and  if she puts the  function inside of another  function:
+ * <ul>
+ * <li>Poles can only be moved from higher altitudes to lower altitudes.</li>
+ * <li>Stacks can only be formed at the initial pole altitudes.</li>
+ * <li>A stack must consist of at least one pole.</li>
+ * <li>The image below shows how poles can be moved into 2 stacks at altitudes 35 and 5.</li>
+ * </ul>
  * <p>
- * min(a, min(b, c))
- * For four integers she can nest the functions once more:
+ * image
+ * Moving the poles down the mountain also costs money. Moving a pole with weight wi and altitude xi to an altitude of
+ * where (xi>xj) costs wi x (xi-xj).
  * <p>
- * min(a, min(b, min(c, d)))
- * Jessica is curious about the structure of these function calls and wants to see if she can write a program to construct a string that shows how  number of integers can be compared with nested  functions. Can you help Jessica write this program?
+ * Write a program to determine the least amount of money needed to rearrange the poles into k stacks.
  * <p>
  * Input Format
  * <p>
- * The input contains a single integer  (the number of integers to be compared).
+ * The first line of input contains two integers  (the number of poles) and  (the number of stacks needed).
+ * <p>
+ * Each of the next n lines include two integers xi indicating the ith pole's altitude and wi indicating the ith pole's weight. The poles will always be listed from lowest to highest altitude.
  * <p>
  * Constraints
  * <p>
  * Output Format
  * <p>
- * Print the string on a single line. Each integer in the string should be written as 'int' and the string must accurately show how  functions can be called to find the smallest of  integers.
+ * Print the minimum cost of rearranging the poles into k stacks.
  * <p>
  * Sample Input 0
  * <p>
- * 2
+ * 3 1
+ * 20 1
+ * 30 1
+ * 40 1
  * Sample Output 0
  * <p>
- * min(int, int)
+ * 30
  * Explanation 0
  * <p>
- * With an input of  we only have two integers to compare. We don't need to nest the  functions for our output because the  function can take two integers as input.
+ * This test case has 3 poles and needs 1 stack. We cannot move the bottom pole to the altitudes above it so
+ * we'll need to move the other poles to the bottom. The cost to move the highest pole to the bottom is 1x[40-20]=20
+ * and the cost to move the middle pole to the bottom is [1x(30-20)]=10. This makes our total cost 30.
  * <p>
  * Sample Input 1
  * <p>
- * 4
+ * 6 2
+ * 10 15
+ * 12 17
+ * 16 18
+ * 18 13
+ * 30 10
+ * 32 1
  * Sample Output 1
  * <p>
- * min(int, min(int, min(int, int)))
+ * 216
  * Explanation 1
  * <p>
- * With  as our input we'll need to compare  integers. We'll call these integers , , , and . The  function can only call two integers at a time so we'll need to call it for the last two integers,  and . We'll refer to this first use of the  function as . We'll call the  function again to compare the result of  with the next integer, . This will be called . We'll finally call the min function again to compare the result of the  with the last number, , bringing us to a total of  calls of the  function, which is shown in the output.
+ * The optimal rearrangement for this test case is to create a stack at altitude 16 and another at altitude 10.
+ * Our final cost will therefore be:
  * <p>
- * If you'd like to test out your output string, implement the  function and call it with a for loop such that each previous result is passed into the next call of the  function.
+ * [1x(32-16)]+[10x(30-16)]+[13x(18-16)]+[17x(12-10)]
+ * 16+140+26+34
+ * 216
  */
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.BiFunction;
 import java.util.stream.IntStream;
 
 public class Solution {
 
     // Solution 2 in Java8
     public static void main(String[] args) {
+        long startMilli = System.currentTimeMillis();
+        long startNano = System.nanoTime();
+
         Scanner in = new Scanner(System.in);
 
         // Gets Inputs
-        int n = in.nextInt(); // number of integers to be compared
+        int n = in.nextInt(); // number of poles
+        int k = in.nextInt(); // number of stacks needed
         // Checks constraints
-        if (n < 2 || n > 50)
-            throw new IllegalArgumentException("The number of integers to be compared should be between 2 and 50.");
+        if (k < 0 || n > 5000 || n < k)
+            throw new IllegalArgumentException("The number of telephone poles and needed stacks should be between " +
+                    "1 and 5000. \n The number of poles should of course greater than the number of stacks needed.");
 
-        System.out.print(repeat(n - 2, new StringBuilder("min(int, ")).append("min(int, int)").append(repeat(n - 2, new StringBuilder(")"))));
+        List<Integer> x_w = new ArrayList<>();
+        IntStream.range(0, n)
+                .forEach(i -> {
+                    int x_i = in.nextInt(); int w_i = in.nextInt();
+                    if (x_i < 0 || x_i > Math.pow(10,6) || w_i < 0 || w_i > Math.pow(10,6))
+                        throw new IllegalArgumentException("The altitude and the weight of pole should be between 1 and 10^6.");
+                    x_w.add(x_i);
+                });
+
+        long endMilli = System.currentTimeMillis();
+        long endNano = System.nanoTime();
+        System.out.println("Time execution in milliSec = " + (endMilli - startMilli));
+        System.out.println("Time execution in nanoSec = " + (endNano - startNano));
+        /*
+        6 2
+10 15
+12 17
+16 18
+18 13
+30 10
+32 1
+         */
     }
 
-    private static StringBuilder repeat(Integer nbNested, StringBuilder out) {
-        StringBuilder outStart = new StringBuilder();
-        IntStream.range(0, nbNested).forEach(v -> outStart.append(out));
-        return outStart;
+    private int delta(int x_i, int x_j){
+        return x_i - x_j;
     }
 
-    /*
-    // Solution 1 in Java8
-    static private StringBuilder outStart = new StringBuilder("");
-
-    public StringBuilder getOutStart() {
-        return outStart;
+    private long cost(int w_i, int delta){
+        return w_i*delta;
     }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Solution solution = new Solution();
-
-        // Gets Inputs
-        int n = in.nextInt(); // number of integers to be compared
-        // Checks constraints
-        if (n < 2 || n > 50)
-            throw new IllegalArgumentException("The number of integers to be compared should be between 2 and 50.");
-
-        System.out.print(solution.repeat(n - 2, new StringBuilder("min(int, ")).repeat(1, new StringBuilder("min(int, int)")).repeat(n - 2, new StringBuilder(")")).getOutStart());
-    }
-
-    private Solution repeat(Integer nbNested, StringBuilder out) {
-        IntStream.range(0,nbNested).forEach(v -> outStart.append(out));
-        return this;
-    }
-     */
-
-    /*
-    // Solution 2 in Java7
-    static private StringBuilder outStart = new StringBuilder("");
-
-    public StringBuilder getOutStart() {
-        return outStart;
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Solution solution = new Solution();
-
-        // Gets Inputs
-        int n = in.nextInt(); // number of integers to be compared
-        // Checks constraints
-        if (n < 2 || n > 50)
-            throw new IllegalArgumentException("The number of integers to be compared should be between 2 and 50.");
-
-        System.out.print(solution.repeat(n - 2, new StringBuilder("min(int, ")).repeat(1, new StringBuilder("min(int, int)")).repeat(n - 2, new StringBuilder(")")).getOutStart());
-    }
-
-    private Solution repeat(Integer nbNested, StringBuilder out) {
-        for (int i = 0; i < nbNested; i++) {
-            outStart.append(out);
-        }
-        return this;
-    }*/
-
-    /*
-        // Solution in Java7:
-        Scanner in = new Scanner(System.in);
-
-        // Gets Inputs
-        int n = in.nextInt(); // number of integers to be compared
-        StringBuilder output = new StringBuilder();
-        StringBuilder outStart = new StringBuilder("min(int, ");
-        StringBuilder outFinalBlock = new StringBuilder("min(int, int)");
-        StringBuilder outEnd = new StringBuilder(")");
-        // Checks constraints
-        if(n<2 || n>50) throw new IllegalArgumentException("The number of integers to be compared should be between 2 and 50.");
-
-        int nbNested = n - 2;
-        for (int i = 0; i <nbNested ; i++) {
-            output.append(outStart);
-        }
-        output.append(outFinalBlock);
-        for (int i = 0; i <nbNested ; i++) {
-            output.append(outEnd);
-        }
-        // Gets Output
-        System.out.print(output);
-        */
 }
